6370afdd23b3da78e8d70354463f6c8642d885ee
obinna <biostechnologyng@gmail.com>
2022-09-22 21:02:32 +0100

    Tuples

---
+++ src/tuples.rs
@@ -0,0 +1,9 @@
+/**
+ * Tuples group together values, they can be  of same or different types
+ * Max of 12 elements
+ */
+pub fn run(){
+    let person:(&str, &str, i8) = ("Obinna", "Mass", 23);
+
+    println!("{} is from {} and he is {} years old", person.0, person.1, person.2);
+}
